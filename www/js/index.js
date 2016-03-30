fe62a7955bdfbb40d2b799e1ec4731fe4d12cd14
Henrique Nogueira <hrqnogueira@gmail.com>
2016-03-29 23:42:16 -0300

    Added photo to mobile app

--- www/js/index.js
+++ www/js/index.js
@@ -7,7 +7,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 
     // To persist coordinates of the mobile
-    var latitude, longitude;
+    var latitude, longitude, photoData = null;
 
     // Position handler: persists
     var onLocationSuccess = function(position) {
@@ -29,23 +29,53 @@ function onDeviceReady() {
         }
     );
 
+    $("#addPhotoButton").click(function () {
+        navigator.camera.getPicture(function(imageData) {
+            photoData = imageData;
+            $('#photo').attr('src', photoData);
+        }, function(err) {
+            alert("Não foi possível adicionar foto.")
+        }, {
+			quality: 50,
+			destinationType: navigator.camera.DestinationType.FILE_URI
+		});
+    });
+
     $(".container a").click(function () {
 
         // Getting report details
         var type = $(this).attr('data-type');
-        var description = $(this).attr('data-description');
 
         // Data to be submitted
-        var data = {
-            category: type,
-            latitude: latitude,
-            longitude: longitude,
-            device_id: device.uuid
-        };
+        var params = new Object();
+        params.category = type;
+        params.latitude = latitude;
+        params.longitude = longitude;
+        params.device_id = device.uuid;
 
-        // Sending report
-        $.post(API_ENDPOINT, data, function (response) {
-            alert('Registrado ' + description + ' na sua posição. Acompanhe os resultados pelo site.');
-        });
+        var options = new FileUploadOptions();
+        options.fileKey = "photo";
+
+        // Image upload
+        if (photoData != null) {
+            options.fileName = photoData.substr(photoData.lastIndexOf('/') + 1);
+            options.mimeType = "image/jpeg";
+            options.chunkedMode = false;
+            options.params = params;
+
+            var ft = new FileTransfer();
+            ft.upload(photoData, API_ENDPOINT, uploadSuccess, uploadFailed, options);
+        }
+        else {
+            $.post(API_ENDPOINT, params, uploadSuccess);
+        }
     });
+
+    function uploadSuccess(response) {
+        alert('Registrada ocorrência na sua posição. Acompanhe os resultados pelo site.');
+    }
+
+    function uploadFailed(err) {
+        alert('Não foi possível enviar o relatório.');
+    }
 }
